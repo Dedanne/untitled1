8c222d35bb86d771b926b7011a972ce50bdcc6f6
Dan Herasimchuk <dangerasimchuk@gmail.com>
2022-07-29 21:16:07 +0300

    Add the numbers

--- src/TEstGit.java
+++ src/TEstGit.java
@@ -2,5 +2,8 @@ public class TEstGit {
     public static void main(String args[]){
         System.out.println("Hello world");
         System.out.println("Hello worl");
+        int a=4;
+        int b=10;
+        int c=a+b;
     }
 }
